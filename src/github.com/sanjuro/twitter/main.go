358d082f975b420844d22aa32c93d1b7f79d1fd6
Shadley Wentzel <shadleywentzel@gmail.com>
2018-06-22 11:31:13 +0200

    Update main.go

--- src/github.com/sanjuro/twitter/main.go
+++ src/github.com/sanjuro/twitter/main.go
@@ -1,12 +1,8 @@
 package main
 
 import (
-	"bufio"
-	"bytes"
 	"fmt"
 	"flag"
-	"io"
-	"os"
 	"sort"
 	"strings"
 )
@@ -101,42 +97,4 @@ func main() {
 			}
 		}
 	}
-}
-
-func checkFile(filename string) bool {
-	if _, err := os.Stat(filename); os.IsNotExist(err) {
-		fmt.Println(fmt.Sprintf("The File %s does not exist", filename))
-		return false
-	} else {
-		return true
-	}
-}
-
-func readLines(path string) (lines []string, err error) {
-	var (
-		file *os.File
-		part []byte
-		prefix bool
-	)
-	if file, err = os.Open(path); err != nil {
-		return
-	}
-	defer file.Close()
-
-	reader := bufio.NewReader(file)
-	buffer := bytes.NewBuffer(make([]byte, 0))
-	for {
-		if part, prefix, err = reader.ReadLine(); err != nil {
-			break
-		}
-		buffer.Write(part)
-		if !prefix {
-			lines = append(lines, buffer.String())
-			buffer.Reset()
-		}
-	}
-	if err == io.EOF {
-		err = nil
-	}
-	return
 }
