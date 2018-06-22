17f1ed05ecd103417136ce5301299eaed6fb3c6d
Shadley Wentzel <shadleywentzel@gmail.com>
2018-06-22 11:30:11 +0200

    Create file_handler.go

---
+++ src/github.com/sanjuro/twitter/file_handler.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+)
+
+func checkFile(filename string) bool {
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		fmt.Println(fmt.Sprintf("The File %s does not exist", filename))
+		return false
+	} else {
+		return true
+	}
+}
+
+func readLines(path string) (lines []string, err error) {
+	var (
+		file *os.File
+		part []byte
+		prefix bool
+	)
+	if file, err = os.Open(path); err != nil {
+		return
+	}
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+	buffer := bytes.NewBuffer(make([]byte, 0))
+	for {
+		if part, prefix, err = reader.ReadLine(); err != nil {
+			break
+		}
+		buffer.Write(part)
+		if !prefix {
+			lines = append(lines, buffer.String())
+			buffer.Reset()
+		}
+	}
+	if err == io.EOF {
+		err = nil
+	}
+	return
+}
